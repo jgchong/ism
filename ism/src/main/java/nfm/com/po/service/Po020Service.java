532c8f287c54663ca5d217342c4765b0b0231488
vaxter0907 <45062305+vaxter0907@users.noreply.github.com>
2018-12-18 09:11:08 +0900

    20181218추가개발오류수정

--- ism/src/main/java/nfm/com/po/service/Po020Service.java
+++ ism/src/main/java/nfm/com/po/service/Po020Service.java
@@ -1,9 +1,14 @@
 package nfm.com.po.service;
 
+import java.util.List;
+
+import nfm.com.byc.service.Ismbyc020VO;
 import nfm.com.po.service.Ismpomsearch020VO;
 
 public interface Po020Service  {
 	public Object selectPoList(Ismpomsearch020VO ismpomsearch020VO) throws Exception;
 	
 	int selectPoListTotCnt(Ismpomsearch020VO ismpomsearch020VO) throws Exception;
+	
+	public Object selectByc020List(String bycId) throws Exception;
 }
