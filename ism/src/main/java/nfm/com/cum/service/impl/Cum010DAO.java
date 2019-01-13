dbfabc8189c026495bcf188d4097507d811846e2
jgchong <42162175+jgchong@users.noreply.github.com>
2019-01-13 17:53:37 +0900

    Update Cum010DAO.java

--- ism/src/main/java/nfm/com/cum/service/impl/Cum010DAO.java
+++ ism/src/main/java/nfm/com/cum/service/impl/Cum010DAO.java
@@ -15,7 +15,7 @@ import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
 public class Cum010DAO extends EgovAbstractDAO {
 	public int selectListTotCnt(Cum010SearchVO cum010SearchVO) {
 		return (Integer)select("cum010DAO.selectListTotCnt", cum010SearchVO);
-	}
+	} 
 
 	public List<?> selectList(Cum010SearchVO cum010SearchVO) {
 		return list("cum010DAO.selectList", cum010SearchVO);
@@ -45,11 +45,19 @@ public class Cum010DAO extends EgovAbstractDAO {
 		delete("cum010DAO.deleteCum030", ismcum010VO);
 	}
 
+	public void updateAllCum030(Ismcum010VO ismcum010VO) {
+		delete("cum010DAO.updateAllCum030", ismcum010VO);
+	}
+
 	public void insertCum020(Ismcum020VO ismcum020VO) {
 		insert("cum010DAO.insertCum020", ismcum020VO);
 	}
 
 	public void insertCum030(Ismcum030VO ismcum030VO) {
 		insert("cum010DAO.insertCum030", ismcum030VO);
+	}
+
+	public void updateCum030(Ismcum030VO ismcum030VO) {
+		insert("cum010DAO.updateCum030", ismcum030VO);
 	}
 }
