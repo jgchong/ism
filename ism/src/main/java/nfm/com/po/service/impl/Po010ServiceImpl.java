e8fd5b7ef181e7fc14c76a0b98fb9ec19029c4b5
vaxter0907 <45062305+vaxter0907@users.noreply.github.com>
2019-01-17 09:43:39 +0900

    20190116 추가개발자 impl upload

--- ism/src/main/java/nfm/com/po/service/impl/Po010ServiceImpl.java
+++ ism/src/main/java/nfm/com/po/service/impl/Po010ServiceImpl.java
@@ -32,7 +32,6 @@ import nfm.com.po.service.Ismpoo010VO;
 import nfm.com.po.service.Ismpoo020VO;
 import nfm.com.po.service.Po010SaveVO;
 import nfm.com.po.service.Po010Service;
-import nfm.com.skd.service.Skd010Service;
 import nfm.com.whs.service.Whs010SearchVO;
 import nfm.com.whs.service.Whs010Service;
 
@@ -75,10 +74,6 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 	/** whs010Service */
 	@Resource(name = "whs010Service")
 	private Whs010Service whs010Service;
-
-	/** Skd010Service */
-	@Resource(name = "skd010Service")
-	private Skd010Service skd010Service;
 	
 	/** po010DAO */
 	@Resource(name="po010DAO")
@@ -346,9 +341,9 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 		    String[] fVal = new String[50];
 		    int fixValueIdx = 0;
 	
-			header.add("key Value");
-			header.add("송장번호");
-			header.add("택배사");
+			//header.add("key Value");
+			//header.add("송장번호");
+			//header.add("택배사");
 			
 		    for(Ismpoo010VO vo : listIsmpoo010VO){
 		    	if ("Y".equals(vo.getIsassign())) {
@@ -370,6 +365,10 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 		    	
 		    	HashMap listKetValueHm = new HashMap();
 		    	
+		    	System.out.println("##########################"+vo.getOrderitemid());
+		    	System.out.println("##########################"+vo.getOrderitemname());
+		    	
+		    	listKetValueHm.put("keyvalue", vo.getOdm010id());
 		    	listKetValueHm.put("pom010id", vo.getPom010id());
 		    	listKetValueHm.put("odm010id", vo.getOdm010id());
 		    	listKetValueHm.put("orderno", vo.getOrderno());
@@ -421,9 +420,9 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 	
 		    	List<Object> obj = new ArrayList<Object>();
 			    
-		    	obj.add(vo.getOdm010id()); //key value
-	    		obj.add("");               //송장번호
-	    		obj.add("");			   //택배사
+		    	//obj.add(vo.getOdm010id()); //key value
+	    		//obj.add("");               //송장번호
+	    		//obj.add("");			   //택배사
 	    		
 	    		for(Ismpoo010VO vosub : listIsmpoo010VO){
 			    	if ("Y".equals(vosub.getIsassign())) {
@@ -530,7 +529,7 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public int setOrderDataFile(File convFile) throws Exception {
+	public int setOrderDataFile(File convFile) {
 
         ExcelReadOption excelReadOption = new ExcelReadOption();
         excelReadOption.setFilePath(convFile.getAbsolutePath());
@@ -555,18 +554,6 @@ public class Po010ServiceImpl extends EgovAbstractServiceImpl implements Po010Se
 	        	System.out.println("##################"+ismodm010VO.getDlvco());
 	        	
 	        	ord020DAO.updateOrderDetailData(ismodm010VO);
-	        	
-	        	Ord020SearchVO ord020SearchVO = new Ord020SearchVO();
-	        	ord020SearchVO.setOdm010id(Integer.parseInt(excelItemInfo.get("A")));
-	        	List<Ismodm010VO> listIsmodm010VO = (List<Ismodm010VO>) ord020Service.selectList(ord020SearchVO);
-	        	String itemcode = "0000000000";
-	        	String itemea = "0";
-
-	    		for (Ismodm010VO item : listIsmodm010VO) {
-	    			itemcode = item.getOrderitemid();
-	    			itemea = item.getOrderitemqty();
-	    		}
-	        	skd010Service.skd010Add(itemcode, null, itemea);
         	}
         }
         return 0;
